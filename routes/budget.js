536e33875319251fb0be38e7b7dbf760724c30c4
Andrew Meng <menga@rose-hulman.edu>
2020-02-05 23:54:23 -0500

    implemented budget page

--- routes/budget.js
+++ routes/budget.js
@@ -10,8 +10,15 @@ router.get('/:id', async function(req, res, next) {
       await sql.connect('mssql://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' 
         + process.env.DB_HOST + '/' + process.env.DB_NAME);
       const result = await sql.query`select budget.year as year, club.name as clubname from budget join club on budget.clubid=club.id where budget.id=${id}`;
-      const lineitems = await sql.query`select l.number, l.originalbalance, l.description, l.name, l.otfr, l.id from lineitem l where l.budgetid=${id}`;
+      const lineitems = await sql.query`EXEC GET_LINEITEMS_FROM_BUDGET @BudgetID_1=${id}`;
       console.log(lineitems);
+      for (lineitem of lineitems.recordset) {
+          if(lineitem.remainingbalance==null) {
+            lineitem.remainingbalance=lineitem.originalbalance;
+            lineitem.deductions=0;
+            console.log(lineitem)
+          }
+      }
       res.render('budget', { title:  result.recordset[0].clubname + "'s " + result.recordset[0].year + " Budget", budget: result.recordset[0], lineitems: lineitems.recordset });
     } catch (err) {
         console.log(err);
